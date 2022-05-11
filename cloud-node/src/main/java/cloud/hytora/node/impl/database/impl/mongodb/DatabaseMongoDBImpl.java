f2b72a04e3a1f3fe8d3407077c24a8564424c4bc
Suqatri <paul@suqatri.net>
2022-05-11 22:51:00 +0200

    Impl mongodb connect & disconnect

--- cloud-node/src/main/java/cloud/hytora/node/impl/database/impl/mongodb/DatabaseMongoDBImpl.java
+++ cloud-node/src/main/java/cloud/hytora/node/impl/database/impl/mongodb/DatabaseMongoDBImpl.java
@@ -2,26 +2,49 @@ package cloud.hytora.node.impl.database.impl.mongodb;
 
 import cloud.hytora.common.wrapper.Wrapper;
 import cloud.hytora.driver.services.configuration.ServerConfiguration;
+import cloud.hytora.node.impl.config.MainConfiguration;
+import cloud.hytora.node.impl.database.DatabaseConfiguration;
 import cloud.hytora.node.impl.database.IDatabase;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.checkerframework.checker.units.qual.C;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class DatabaseMongoDBImpl implements IDatabase {
 
+    private MongoClient client;
+    private MongoDatabase database;
 
     @Override
     public void connect() {
+        DatabaseConfiguration configuration = MainConfiguration.getInstance().getDatabaseConfiguration();
+        String uri = null;
+        if(configuration.getUser().isEmpty() && configuration.getPassword().isEmpty()){
+            uri = "mongodb://" + configuration.getHost() + ":" + configuration.getPort() + "/?authSource=" + configuration.getAuthDatabase();
+        }else{
+            uri = "mongodb://" + configuration.getUser() + ":" + configuration.getPassword() + "@" + configuration.getHost() + ":" + configuration.getPort() + "/?authSource=" + config.getAuthDatabase();
+        }
+        ConnectionString connectionString = new ConnectionString(uri);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        this.client = MongoClients.create(settings);
+        this.database = this.client.getDatabase(configuration.getDatabase());
     }
 
     @Override
     public @NotNull Wrapper<Boolean> disconnect() {
-        return null;
-    }
-
-    @Override
-    public List<ServerConfiguration> getAllServiceGroups() {
-        return null;
+        Wrapper<Boolean> shutdownPromise = Wrapper.empty();
+        if(this.client != null) {
+            this.client.close();
+        }
+        shutdownPromise.setResult(true);
+        return shutdownPromise;
     }
 
     @Override
@@ -33,4 +56,10 @@ public class DatabaseMongoDBImpl implements IDatabase {
     public void removeGroup(@NotNull ServerConfiguration serviceGroup) {
 
     }
+
+    @Override
+    public List<ServerConfiguration> getAllServiceGroups() {
+        return null;
+    }
+
 }
