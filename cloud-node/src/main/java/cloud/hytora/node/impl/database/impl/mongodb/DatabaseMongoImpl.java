43a2a6177325908f2226c544b48df67e5b4a4f81
Suqatri <paul@suqatri.net>
2022-05-11 22:44:38 +0200

    Rename DatabaseMongoImpl

--- cloud-node/src/main/java/cloud/hytora/node/impl/database/impl/mongodb/DatabaseMongoImpl.java
+++ cloud-node/src/main/java/cloud/hytora/node/impl/database/impl/mongodb/DatabaseMongoImpl.java
@@ -1,16 +1,19 @@
-package cloud.hytora.node.impl.database.impl.mongo;
+package cloud.hytora.node.impl.database.impl.mongodb;
 
 import cloud.hytora.common.wrapper.Wrapper;
 import cloud.hytora.driver.services.configuration.ServerConfiguration;
+import cloud.hytora.node.impl.config.MainConfiguration;
+import cloud.hytora.node.impl.database.DatabaseConfiguration;
 import cloud.hytora.node.impl.database.IDatabase;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class DatabaseMongoImpl implements IDatabase {
+
+
     @Override
     public void connect() {
-
     }
 
     @Override
